554dd43cf47d3e7fc5e486ed11205acb449a9c23
Valentina Unzhakova <vunzhakova@outlook.com>
2022-03-31 12:04:36 +0300

    commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const bodyChangeEl = document.querySelector('body');
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+let intervalId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+buttonStop.setAttribute('disabled', '');
+
+buttonStart.addEventListener('click', element => {
+  element.target.setAttribute('disabled', true);
+  buttonStop.removeAttribute('disabled');
+
+  intervalId = setInterval(() => {
+    bodyChangeEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+buttonStop.addEventListener('click', element => {
+  element.target.setAttribute('disabled', true);
+  buttonStart.removeAttribute('disabled');
+
+  clearInterval(intervalId);
+});
